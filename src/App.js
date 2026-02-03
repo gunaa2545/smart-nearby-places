57f9c42eb0a88013dd551c835586c50a3a4f8942
moturigunasri <moturigunasri@gmail.com>
2026-02-03 23:20:09 +0530

    Added mood selection dropdown

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  const [mood, setMood] = useState("");
+
   return (
     <div>
       <h1>Smart Nearby Places</h1>
-      <p>My first project is running 🎉</p>
+
+      <select onChange={(e) => setMood(e.target.value)}>
+        <option value="">Select your mood</option>
+        <option value="work">Work</option>
+        <option value="date">Date</option>
+        <option value="quick">Quick Bite</option>
+        <option value="budget">Budget</option>
+      </select>
+
+      <p>Selected mood: {mood}</p>
     </div>
   );
 }
 
 export default App;
-
